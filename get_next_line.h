a74ebd63b720224257ad7a582e9b7cad42553042
ArtsAlexander <aarts@student.s19.be>
2021-05-27 13:51:43 +0200

    gnl start

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarts <aarts@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/26 14:08:40 by aarts             #+#    #+#             */
+/*   Updated: 2021/05/26 14:10:34 by aarts            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+#define GET_NEXT_LINE
+
+#include <stdlib.h>
+#include <limits.h>
+#include <
+
+int	get_next_line(int fd, char **line);
+
+#endif
