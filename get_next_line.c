01b97a8950e72fc5deebc2b916749a95763e129a
ArtsAlexander <aarts@student.s19.be>
2021-07-01 20:41:28 +0200

    hello, vim suck, vs better

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,28 @@
 /*   By: aarts <aarts@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 13:12:15 by aarts             #+#    #+#             */
-/*   Updated: 2021/07/01 19:10:48 by aarts            ###   ########.fr       */
+/*   Updated: 2021/07/01 20:38:09 by aarts            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static int	freeandreturn(char *str)
+{
+	free(str);
+	str = NULL;
+	return (-1);
+}
+
+static int	putloop(char *line, char *saved_fd)
+{
+	*line = str_dup(*saved_fd);
+	if (!(*line))
+		return(freeandreturn(*line));
+	free(*saved_fd);
+	*saved_fd = NULL;
+	return (0);
+}
 
 static int	putinline(char **saved_fd, char **line)
 {
@@ -23,19 +40,19 @@ static int	putinline(char **saved_fd, char **line)
 	if ((*saved_fd)[i] == '\n')
 	{
 		*line = sub_str((*saved_fd), i);
+		if (!(*line))
+			return(freeandreturn(*line));
 		temp = str_dup(&(*saved_fd)[i + 1]);
+		if (!temp)
+			return(freeandreturn(temp));
 		free(*saved_fd);
 		*saved_fd = temp;
 	}
 	else
-	{
-		*line = str_dup(*saved_fd);
-		free(*saved_fd);
-		*saved_fd = NULL;
-		return (0);
-	}
+		return(putloop(line, saved_fd));
 	return (1);
 }
+
 
 int	get_next_line(int fd, char **line)
 {
@@ -52,10 +69,16 @@ int	get_next_line(int fd, char **line)
 		ret = read(fd, buf, BUFFER_SIZE);
 		buf[ret] = '\0';
 		if (saved[fd] == NULL)
+		{
 			saved[fd] = str_dup(buf);
+			if (!saved[fd])
+				return(freeandreturn(saved[fd]));
+		}
 		else
 		{
 			tmp = str_join(saved[fd], buf);
+			if (!tmp)
+				return(freeandreturn(tmp));
 			free(saved[fd]);
 			saved[fd] = tmp;
 		}
---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+int        main(int ac, char **av)
+{
+    char    *line;
+    int        nb_line;
+    int        fd;
+
+    if (ac < 1)
+        return (0);
+    nb_line = 1;
+    if ((fd = open(av[1], O_RDONLY)) == -1)
+        printf("le fichier n'existe pas.");
+    line = NULL;
+    int ret = 0;
+    while ((ret = get_next_line(fd, &line)) > 0 && nb_line)
+    {
+        printf("return-value[%d]: %s\n", ret, line);
+        free(line);
+        line = NULL;
+        nb_line++;
+    }
+    printf("return-value[%d]: %s\n", ret, line);
+    free(line);
+    line = NULL;
+}
