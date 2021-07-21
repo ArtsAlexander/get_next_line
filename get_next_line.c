e49dbe93394942b55aee8a9fda680a50bccfb801
Alexander Arts <aarts@student.s19.student.s>
2021-07-21 19:12:57 +0200

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,32 @@
 /*   By: aarts <aarts@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 13:12:15 by aarts             #+#    #+#             */
-/*   Updated: 2021/07/21 17:24:27 by aarts            ###   ########.fr       */
+/*   Updated: 2021/07/21 18:55:23 by aarts            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-static char	*putinline(char **saved_fd, char **line)
+static char	*ft_free(char *str)
+{
+	free(str);
+	return (NULL);
+}
+
+static char	*putinline2(char **saved_fd, char *line)
+{
+	line = str_dup(*saved_fd);
+	if (!line)
+		return (ft_free(line));
+	free(*saved_fd);
+	*saved_fd = NULL;
+	return (line);
+}
+
+static char	*putinline(char **saved_fd)
 {
 	char	*temp;
+	char	*line;
 	size_t	i;
 
 	i = 0;
@@ -23,22 +39,18 @@ static char	*putinline(char **saved_fd, char **line)
 		i++;
 	if ((*saved_fd)[i] == '\n')
 	{
-		*line = sub_str((*saved_fd), i + 1);
+		line = sub_str((*saved_fd), i + 1);
+		if (!line)
+			return (ft_free(line));
 		temp = str_dup(&(*saved_fd)[i + 1]);
-		if (!temp || !(*line))
-			return (ft_free());
+		if (!temp)
+			return (ft_free(temp));
 		free(*saved_fd);
 		*saved_fd = temp;
 	}
 	else
-	{
-		*line = str_dup(*saved_fd);
-		if (*line)
-			return (ft_free(*line))
-		free(*saved_fd);
-		*saved_fd = NULL;
-	}
-	return(*line);
+		return (putinline2(saved_fd, line));
+	return(line);
 }
 
 char	*get_next_line(int fd)
@@ -46,10 +58,8 @@ char	*get_next_line(int fd)
 	static char	*saved[FD_MAX];
 	char		buf[BUFFER_SIZE + 1];
 	char		*tmp;
-	char		*line;
 	int			ret;
 	
-	printf("--%s--", saved[fd]);
 	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, buf, 0) == -1)
 		return (NULL);
 	ret = 1;
@@ -59,22 +69,20 @@ char	*get_next_line(int fd)
 		buf[ret] = '\0';
 		if (saved[fd] == NULL)
 		{
-			if (!buf[0])
-				return (NULL);
 			saved[fd] = str_dup(buf);
-			if (!saved[fd])
-				return (NULL);
+			if (!saved[fd] || ret == 0)
+				return (ft_free(saved[fd]));
 		}
 		else
 		{
 			tmp = str_join(saved[fd], buf);
 			if (!tmp)
-				return (NULL);
+				return (ft_free(tmp));
 			free(saved[fd]);
 			saved[fd] = tmp;
 		}
 		if (str_chr(saved[fd], '\n'))
 			break ;
 	}
-	return (putinline(&saved[fd], &line));
+	return (putinline(&saved[fd]));
 }
